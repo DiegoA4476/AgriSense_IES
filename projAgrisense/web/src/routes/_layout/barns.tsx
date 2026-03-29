3bf0f5b8975f08abafd2659f6191d1f289e4fcb8
goni5847 <gonmonfon@gmail.com>
2026-03-29 17:38:29 +0100

    Barn Card

--- projAgrisense/web/src/routes/_layout/barns.tsx
+++ projAgrisense/web/src/routes/_layout/barns.tsx
@@ -2,8 +2,10 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import { Field } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { Plus } from "lucide-react";
+import {  Plus, Trash } from "lucide-react";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { Card,  CardFooter, CardTitle } from "@/components/ui/card";
+
 export const Route = createFileRoute("/_layout/barns")({
   component: RouteComponent,
 });
@@ -32,8 +34,56 @@ function RouteComponent() {
             {isMobile ? "" : "New Barn"}
           </Button>
         </Field>
-        <div>
-          <span>put cards here</span>
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-2">
+             <Card size="sm" className="flex flex-row px-4 justify-between items-center">
+                <CardTitle className="w-fit">Barn 1</CardTitle>
+                <CardFooter className="p-0! gap-2">
+                  <span className="top-2 right-2 h-4 w-4 rounded-full bg-destructive text-white text-[10px] font-medium flex items-center justify-center">3</span>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
+                  >
+                    <Trash className="h-3.5 w-3.5" />
+                  </Button>
+                </CardFooter>
+            </Card>
+             <Card size="sm" className="flex flex-row px-4 justify-between items-center">
+                <CardTitle className="w-fit">Barn 2</CardTitle>
+                <CardFooter className="p-0! gap-2">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
+                  >
+                    <Trash className="h-3.5 w-3.5" />
+                  </Button>
+                </CardFooter>
+            </Card>
+            <Card size="sm" className="flex flex-row px-4 justify-between items-center">
+                <CardTitle className="w-fit">Barn 3</CardTitle>
+                <CardFooter className="p-0! gap-2">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
+                  >
+                    <Trash className="h-3.5 w-3.5" />
+                  </Button>
+                </CardFooter>
+            </Card>
+            <Card size="sm" className="flex flex-row px-4 justify-between items-center">
+                <CardTitle className="w-fit">Barn 4</CardTitle>
+                <CardFooter className="p-0! gap-2">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
+                  >
+                    <Trash className="h-3.5 w-3.5" />
+                  </Button>
+                </CardFooter>
+            </Card>
         </div>
       </div>
     </div>
