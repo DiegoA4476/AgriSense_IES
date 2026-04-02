363a61675cb654235660ed12e26362f26fc0a482
goni5847 <gonmonfon@gmail.com>
2026-04-02 12:44:41 +0100

    Barn and Delete Barn modals

--- projAgrisense/web/src/routes/_layout/barns.tsx
+++ projAgrisense/web/src/routes/_layout/barns.tsx
@@ -2,16 +2,16 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import { Field } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import {  Plus, Trash } from "lucide-react";
-import { useIsMobile } from "@/hooks/use-mobile";
+import { Trash } from "lucide-react";
 import { Card,  CardFooter, CardTitle } from "@/components/ui/card";
+import { BarnModal } from "@/components/custom/barn-modal";
+import { DeleteBarnModal } from "@/components/custom/delete-barn-modal";
 
 export const Route = createFileRoute("/_layout/barns")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const isMobile = useIsMobile();
   return (
     <div className="px-8 py-7 md:px-28 md:py-14 flex flex-col h-full overflow-hidden">
       <div className="flex justify-center w-full mb-5 md:mb-10">
@@ -29,59 +29,40 @@ function RouteComponent() {
             placeholder="Search by name..."
             className="max-w-2xs md:max-w-188 bg-[#FFFFFF]"
           />
-          <Button className="bg-[#16A34A] cursor-pointer">
-            <Plus />
-            {isMobile ? "" : "New Barn"}
-          </Button>
+          <BarnModal/>
         </Field>
         <div className="grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-2">
              <Card size="sm" className="flex flex-row px-4 justify-between items-center">
                 <CardTitle className="w-fit">Barn 1</CardTitle>
                 <CardFooter className="p-0! gap-2">
                   <span className="top-2 right-2 h-4 w-4 rounded-full bg-destructive text-white text-[10px] font-medium flex items-center justify-center">3</span>
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
-                  >
-                    <Trash className="h-3.5 w-3.5" />
-                  </Button>
+                    <DeleteBarnModal
+                      barnName="Barn 1" onConfirm={function (): void {} }
+                    />
                 </CardFooter>
             </Card>
              <Card size="sm" className="flex flex-row px-4 justify-between items-center">
                 <CardTitle className="w-fit">Barn 2</CardTitle>
                 <CardFooter className="p-0! gap-2">
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
-                  >
-                    <Trash className="h-3.5 w-3.5" />
-                  </Button>
+                  <DeleteBarnModal
+                      barnName="Barn 2" onConfirm={function (): void {} }
+                    />
                 </CardFooter>
             </Card>
             <Card size="sm" className="flex flex-row px-4 justify-between items-center">
                 <CardTitle className="w-fit">Barn 3</CardTitle>
                 <CardFooter className="p-0! gap-2">
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
-                  >
-                    <Trash className="h-3.5 w-3.5" />
-                  </Button>
+                  <DeleteBarnModal
+                      barnName="Barn 3" onConfirm={function (): void {} }
+                    />
                 </CardFooter>
             </Card>
             <Card size="sm" className="flex flex-row px-4 justify-between items-center">
                 <CardTitle className="w-fit">Barn 4</CardTitle>
                 <CardFooter className="p-0! gap-2">
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    className="h-8 w-8 text-destructive bg-destructive/10 md:text-muted-foreground md:bg-transparent hover:text-destructive hover:bg-destructive/10 transition-colors"
-                  >
-                    <Trash className="h-3.5 w-3.5" />
-                  </Button>
+                  <DeleteBarnModal
+                      barnName="Barn 4" onConfirm={function (): void {} }
+                    />
                 </CardFooter>
             </Card>
         </div>
