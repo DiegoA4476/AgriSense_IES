20acd209a38fe6a973c87fdf7b6b46b320b94e86
goni5847 <gonmonfon@gmail.com>
2026-04-04 18:10:48 +0100

    Swagger working

--- projAgrisense/api/src/main/java/ua/ies/api/controller/BarnController.java
+++ projAgrisense/api/src/main/java/ua/ies/api/controller/BarnController.java
@@ -1,5 +1,7 @@
 package ua.ies.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,25 +13,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/barns")
 @RequiredArgsConstructor
+@Tag(name = "Barns", description = "CRUD operations for barns")
 public class BarnController {
 
     private final BarnService barnService;
 
+    @Operation(summary = "Get all barns")
     @GetMapping
     public List<BarnDTO> getAll() {
         return barnService.findAll();
     }
 
+    @Operation(summary = "Get barn by ID")
     @GetMapping("/{id}")
     public BarnDTO getById(@PathVariable Long id) {
         return barnService.findById(id);
     }
 
+    @Operation(summary = "Create a barn")
     @PostMapping
     public BarnDTO create(@RequestBody BarnDTO dto) {
         return barnService.create(dto);
     }
 
+    @Operation(summary = "Delete a barn")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         barnService.delete(id);
